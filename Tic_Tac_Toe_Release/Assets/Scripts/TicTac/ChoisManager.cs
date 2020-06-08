0f1b1ec1d3c581cde2f608d3db9483964fa18196
ROMAN DEMIDOV <roman199798@gmail.com>
2020-06-08 15:05:00 +0300

    Non

---
+++ Tic_Tac_Toe_Release/Assets/Scripts/TicTac/ChoisManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoisManager : MonoBehaviour
+{
+
+    private bool FirstPlayer;
+
+    public ChoisManager Invock { get { return this; } }
+
+    private void Awake()
+    {
+
+        FirstPlayer = true;
+    }
+
+}
